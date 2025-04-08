aa1e8533e1b220503c78927576df7dfd10ae23d9
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-08 15:49:11 +0200

    everything is showing up from tghe api besides the types

--- script.js
+++ script.js
@@ -16,7 +16,23 @@ function renderPkmCard(){
     let pkmCardsContainer = document.getElementById("content")
     pkmCardsContainer.innerHTML = "";
     for (let i = 0; i < pokemonArray.length; i++) {
-        pkmCardsContainer.innerHTML += `<div class="poke_card"></div>`
+        pkmCardsContainer.innerHTML += `
+        <div class="poke_card">
+            <p>ID:  #${pokemonArray[i].id}</p>
+            <h3>${pokemonArray[i].name}</h3>
+            <img src=${pokemonArray[i].sprites.other.showdown.front_shiny}>
+            <div id="type"></div>
+        </div>`
+        let typeContainer = document.getElementById("type")
+        typeContainer = "";
+        for (let j = 0; j < pokemonArray[i].types.length; j++) {
+            const types = pokemonArray[i].types[j].type.name
+            console.log(pokemonArray[i].types);
+            typeContainer.innerHTML += `
+            <p>${types}</p>`
+            
+            
+        }
     }
     console.log(pokemonArray)
 }
