ffe3495003b43217be9b2101c02037c5f5f366c1
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-08 14:21:21 +0200

    turned the objects into an array

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let startIndex = 1
-let lastIndex = 20
+let startIndex = 1;
+let lastIndex = 20;
+let pokemonArray = [];
 
 async function fetchUrls() {
     for (let i = startIndex; i < lastIndex; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url)
         let currentJSON = await response.json();
+        pokemonArray.push(currentJSON)
         renderPkmCard(currentJSON)
     }
 }
@@ -14,12 +16,8 @@ function renderPkmCard(currentJSON){
     let pkmCardsContainer = document.getElementById("content")
     pkmCardsContainer.innerHTML = "";
     for (let i = 0; i < currentJSON.length; i++) {
-        const currentPkm = currentJSON[i];
-        console.log(currentJSON[i]);
+        console.log(currentJSON);
         
-        pkmCardsContainer.innerHTML += `
-        <p>currentPkm</p>
-        `
     }
 }
 
