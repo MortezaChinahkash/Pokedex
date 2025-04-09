1e940a43bbff90a38e102d632a0ee6b5348def43
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-09 02:14:14 +0200

    setting up the overlay

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function renderPkmCard(){
         let typeID = `type-${i}`
         let pokeCardIndex = `pokeCard${i}`
         pkmCardsContainer.innerHTML += `
-        <div id="${pokeCardIndex}" class="poke_card">
+        <div onclick="renderOverlay(${i})" id="${pokeCardIndex}" class="poke_card">
             <p>ID:  #${pokemonArray[i].id}</p>
             <h3>${pokemonArray[i].name}</h3>
             <img src=${pokemonArray[i].sprites.other.showdown.front_shiny}>
@@ -40,4 +40,48 @@ function addMorePokemon() {
     startIndex = lastIndex;
     lastIndex += 20;
     fetchUrls();
+}
+
+function renderOverlay(i){
+    document.getElementById("dialogueWindow").classList.add("overlay");
+    document.getElementById("dialogueWindow").classList.remove("d_none");
+    updateOverlay(i);
+    }
+
+    function updateOverlay(i) {
+            let dialogueWindow = document.getElementById("dialogueWindow");
+            dialogueWindow.innerHTML = `
+            <div class="overlay_innerwindow" id="overlayInnerWindow">
+                <div class="overlay_header">
+                    <h1>${pokemonArray[i].name}</h1>
+                    <img class="img_btn" onclick="closeOverlay()" src="./assets/png/close.svg">
+                </div>
+                <img class="pkmn_overlay" src=${pokemonArray[i].sprites.other.showdown.front_shiny}>
+                <div class="navigation_button d_flex flex_y_center">
+                    <button onclick="previousPkmn(${i})" class="button_leftright"> <- </button>
+                    <p>${i +1}/${pokemonArray.length}</p>
+                    <button onclick="nextPkmn(${i})" class="button_leftright"> -> </button>
+                </div>
+            </div>`;
+    }
+
+function closeOverlay(){
+    document.getElementById("dialogueWindow").classList.remove("overlay");
+    document.getElementById("dialogueWindow").classList.add("d_none");
+    document.getElementById("overlayInnerWindow").classList.add("d_none")
+}
+function previousPkmn(i) {
+    if (i > 0) {
+        updateOverlay(i - 1);
+    } else {
+        updateOverlay(pokemonArray.length - 1);
+    }
+}
+
+function nextPkmn(i) {
+    if (i < pokemonArray.length - 1) {
+        updateOverlay(i + 1);
+    } else {
+        updateOverlay(0);
+    }
 }
