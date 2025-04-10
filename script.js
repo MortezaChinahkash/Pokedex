5a910eda3d1f5e44f1d7300150d8272a339f6ad7
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-10 18:17:57 +0200

    added the types and a button to play the screams

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let startIndex = 1;
 let lastIndex = 21;
 let pokemonArray = [];
 let maxStats = {
-    hp: 255,
-    attack: 190,
-    defense: 250,
-    'special-attack': 154,
-    'special-defense': 250,
-    speed: 150
-  };
+  hp: 255,
+  attack: 190,
+  defense: 250,
+  "special-attack": 154,
+  "special-defense": 250,
+  speed: 150,
+};
 
 function renderOverlay(i) {
   let pokeTypeBg = pokemonArray[i].types[0].type.name;
@@ -19,38 +19,39 @@ function renderOverlay(i) {
 
 function updateOverlay(i, pokeTypeBg) {
   let dialogueWindow = document.getElementById("dialogueWindow");
-  for (let j = 0; j < pokemonArray[i].types.length; j++) {
-    let pokeType = pokemonArray[i].types[j].type.name;
-  }
+
   dialogueWindow.innerHTML = `
-            <div class="overlay_innerwindow" id="overlayInnerWindow">
-                <div class="overlay_header">
-                <div class="d_flex flex_y_center">
-                    <div onclick="renderGeneralStats(${i})" class="btn overlay_btn">General stats</div>
-                    <div onclick="renderBaseStats(${i})" class="btn overlay_btn">Base stats</div>
-                    <div onclick="renderAttacks(${i})" class="btn overlay_btn">Attacks</div>
+    <div class="overlay_innerwindow" id="overlayInnerWindow">
+        <div class="overlay_header">
+            <div class="d_flex flex_y_center">
+                <div onclick="renderGeneralStats(${i})" class="btn overlay_btn">General stats</div>
+                <div onclick="renderBaseStats(${i})" class="btn overlay_btn">Base stats</div>
+                <div onclick="renderAttacks(${i})" class="btn overlay_btn">Attacks</div>
+            </div>
+            <img class="img_btn" onclick="closeOverlay()" src="./assets/png/close.svg">
+        </div>
+        <div class="pokemon_info">
+            <div class="flex_column">
+                <h1>${pokemonArray[i].name}</h1>
+                <div class="pokemon_card_bg">
+                    <img class="pkmn_overlay" src=${pokemonArray[i].sprites.other.showdown.front_shiny}>
                 </div>
-                    <img class="img_btn" onclick="closeOverlay()" src="./assets/png/close.svg">
-                </div>
-                <div class="pokemon_info">
-                    <div class="flex_column">
-                        <h1>${pokemonArray[i].name}</h1>
-                        <div class="pokemon_card_bg">
-                            <img class="pkmn_overlay" src=${pokemonArray[i].sprites.other.showdown.front_shiny}>
-                        </div>
-                    </div>
-                    <div>
-                        <h1 class="stats">Pokemon Stats</h1>
-                        <div id="differentStats"></div>
-                    </div>`;
-                    <div id="typesInOverlay"></div>
-
-
-
+            </div>
+            <div>
+                <h1 class="stats">Pokemon Stats</h1>
+                <div id="differentStats"></div>
+            </div>
+        </div>
+        <div class="types_scream">
+          <div id="typesInOverlayandScream"></div>
+          <button class="scream_button">Test</button>
+        </div>
+    </div>
+`;
   let overlayBg = document.getElementById("overlayInnerWindow");
   addTypeBgOverlay(overlayBg, pokeTypeBg);
   renderGeneralStats(i);
-  addTypesToOverlay(i, pokeType)
+  addTypesToOverlay(i);
 }
 
 function closeOverlay() {
@@ -189,61 +190,66 @@ function renderGeneralStats(i) {
 }
 
 function renderBaseStats(i) {
-    let statsContainer = document.getElementById("differentStats");
-    let baseStats = pokemonArray[i].stats;
-    statsContainer.innerHTML = "";
-    let cardHTML = `
+  let statsContainer = document.getElementById("differentStats");
+  let baseStats = pokemonArray[i].stats;
+  statsContainer.innerHTML = "";
+  let cardHTML = `
       <div class="card" id="pokemon-card">
         <div class="progress_bars">`;
-        renderProgressbar(statsContainer, baseStats, cardHTML)
+  renderProgressbar(statsContainer, baseStats, cardHTML);
 }
 
-function renderProgressbar(statsContainer, baseStats, cardHTML){
-    for (let k = 0; k < baseStats.length; k++) {
-      let statName = baseStats[k].stat.name;
-      let baseValue = baseStats[k].base_stat;
-      let maxValue = maxStats[statName];
-      cardHTML += `
+function renderProgressbar(statsContainer, baseStats, cardHTML) {
+  for (let k = 0; k < baseStats.length; k++) {
+    let statName = baseStats[k].stat.name;
+    let baseValue = baseStats[k].base_stat;
+    let maxValue = maxStats[statName];
+    cardHTML += `
   <div class="progress_bar_container">
-    <span class="stat-label">${capitalize(statName)}: ${baseValue} / ${maxValue}</span>
+    <span class="stat-label">${capitalize(
+      statName
+    )}: ${baseValue} / ${maxValue}</span>
     <div id="${statName}-bar" class="progress_bar"></div>
   </div>`;
   }
-    statsContainer.innerHTML = cardHTML;
-    for (let k = 0; k < baseStats.length; k++) {
-      let statName = baseStats[k].stat.name;
-      let baseValue = baseStats[k].base_stat;
-      let maxValue = maxStats[statName];
-      updateProgressBar(`${statName}-bar`, baseValue, maxValue);
-    }
+  statsContainer.innerHTML = cardHTML;
+  for (let k = 0; k < baseStats.length; k++) {
+    let statName = baseStats[k].stat.name;
+    let baseValue = baseStats[k].base_stat;
+    let maxValue = maxStats[statName];
+    updateProgressBar(`${statName}-bar`, baseValue, maxValue);
   }
+}
 
-  function updateProgressBar(id, value, maxValue) {
-    let percentage = (value / maxValue) * 100;
-    let bar = document.getElementById(id);
-    bar.style.width = percentage + '%';
-  }
+function updateProgressBar(id, value, maxValue) {
+  let percentage = (value / maxValue) * 100;
+  let bar = document.getElementById(id);
+  bar.style.width = percentage + "%";
+}
 
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-  function renderAttacks(i) {
-    let statsContainer = document.getElementById("differentStats");
-    let attacks = pokemonArray[i].moves
-    statsContainer.innerHTML = ""
-    statsContainer.innerHTML = `<div class="card_attacks" id="pokemon_attacks"></div>`
-    for (let l = 0; l < attacks.length; l++) {
-      document.getElementById("pokemon_attacks").innerHTML += /*html*/ `
+function renderAttacks(i) {
+  let statsContainer = document.getElementById("differentStats");
+  let attacks = pokemonArray[i].moves;
+  statsContainer.innerHTML = "";
+  statsContainer.innerHTML = `<div class="card_attacks" id="pokemon_attacks"></div>`;
+  for (let l = 0; l < attacks.length; l++) {
+    document.getElementById("pokemon_attacks").innerHTML += /*html*/ `
       <div class="attacks">
       <p>${attacks[l].move.name}</p>
-      </div>`
-    }
+      </div>`;
   }
+}
 
-  function addTypesToOverlay(i, pokeType){
-    let typesContainer = document.getElementById("typesInOverlay")
+function addTypesToOverlay(i) {
+  let typesContainer = document.getElementById("typesInOverlayandScream");
+  for (let j = 0; j < pokemonArray[i].types.length; j++) {
+    let pokeType = pokemonArray[i].types[j].type.name;
     typesContainer.innerHTML += `
-    
-    `
-  }
+      <img class="type_pic" src="./assets/png/overlay_types/${pokeType}.svg">
+    `;
+}
+}
